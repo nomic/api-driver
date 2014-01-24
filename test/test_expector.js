3166e2f14ddeae4c7554c2bccd78f795c10b921b
Kenneth Gunn <ken@nomic.com>
2014-01-23 18:08:10 -0800

    get some tests

--- test/test_expector.js
+++ test/test_expector.js
@@ -6,5 +6,25 @@ var assert = require('assert'),
     checkJSONExpression = expector.test.checkJSONExpression;
 
 
-suite('Test checkJSONExpression', function(done) {
+suite('checkJSONExpression', function(done) {
+    test('$length', function(done) {
+        assert(checkJSONExpression({$length: 1}, [2]));
+        assert(!checkJSONExpression({$length: 1}, []));
+        assert(!checkJSONExpression({$length: 1}, [2, 3]));
+        assert(checkJSONExpression({$length: 4}, [1, 2, 3, 4]));
+
+        done();
+    });
+
+    test('$unordered', function(done) {
+        assert(checkJSONExpression({$unordered: [1, 2]}, [1, 2]));
+        assert(checkJSONExpression({$unordered: [1, 2]}, [2, 1]));
+
+        // FIXME: this seems like it should fail
+        //assert(!checkJSONExpression({$unordered: [1, 2]}, [1, 2, 3]));
+
+        assert(!checkJSONExpression({$unordered: [1, 2, 3]}, [1, 2]));
+
+        done();
+    });
 });
