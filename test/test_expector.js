9f5ba53a83bde1c310b40b7e664eda21d00021ea
Kenneth Gunn <ken@nomic.com>
2014-01-23 17:59:00 -0800

    get test bits in place for checkJSONExpression

---
+++ test/test_expector.js
@@ -0,0 +1,10 @@
+"use strict";
+/*global suite: false, test: false, setup: false*/
+
+var assert = require('assert'),
+    expector = require('../lib/expector'),
+    checkJSONExpression = expector.test.checkJSONExpression;
+
+
+suite('Test checkJSONExpression', function(done) {
+});
