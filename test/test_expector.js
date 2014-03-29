9ce553579c9f6c46a6a4234915544d9b5a973a38
Aaron Boyd <aaronyo@yahoo.com>
2014-03-29 02:07:05 +0000

    abbreviating repeated fn checkJSONExpression

--- test/test_expector.js
+++ test/test_expector.js
@@ -3,47 +3,47 @@
 
 var assert = require('assert'),
     expector = require('../lib/expector'),
-    checkJSONExpression = expector.test.checkJSONExpression;
+    cje = expector.test.checkJSONExpression;
 
 
-suite('checkJSONExpression', function(done) {
+suite('checkJSONExpresion', function() {
     test('$length', function(done) {
-        assert(checkJSONExpression({$length: 1}, [2]));
-        assert(!checkJSONExpression({$length: 1}, []));
-        assert(!checkJSONExpression({$length: 1}, [2, 3]));
-        assert(checkJSONExpression({$length: 4}, [1, 2, 3, 4]));
+        assert(cje({$length: 1}, [2]));
+        assert(!cje({$length: 1}, []));
+        assert(!cje({$length: 1}, [2, 3]));
+        assert(cje({$length: 4}, [1, 2, 3, 4]));
 
         done();
     });
 
     test('$unordered', function(done) {
-        assert(checkJSONExpression({$unordered: [1, 2]}, [1, 2]));
-        assert(checkJSONExpression({$unordered: [1, 2]}, [2, 1]));
+        assert(cje({$unordered: [1, 2]}, [1, 2]));
+        assert(cje({$unordered: [1, 2]}, [2, 1]));
 
         // FIXME: this seems like it should fail
-        //assert(!checkJSONExpression({$unordered: [1, 2]}, [1, 2, 3]));
+        //assert(!cje({$unordered: [1, 2]}, [1, 2, 3]));
 
-        assert(!checkJSONExpression({$unordered: [1, 2, 3]}, [1, 2]));
+        assert(!cje({$unordered: [1, 2, 3]}, [1, 2]));
 
         done();
     });
 
     test('comparisons', function(done) {
-        assert( checkJSONExpression({a: {$gt:  10}}, {a: 11}));
-        assert(!checkJSONExpression({a: {$gt:  10}}, {a: 10}));
-        assert(!checkJSONExpression({a: {$gt:  10}}, {a: 9}));
+        assert( cje({a: {$gt:  10}}, {a: 11}));
+        assert(!cje({a: {$gt:  10}}, {a: 10}));
+        assert(!cje({a: {$gt:  10}}, {a: 9}));
 
-        assert( checkJSONExpression({a: {$gte: 10}}, {a: 11}));
-        assert( checkJSONExpression({a: {$gte: 10}}, {a: 10}));
-        assert(!checkJSONExpression({a: {$gte: 10}}, {a: 9}));
+        assert( cje({a: {$gte: 10}}, {a: 11}));
+        assert( cje({a: {$gte: 10}}, {a: 10}));
+        assert(!cje({a: {$gte: 10}}, {a: 9}));
 
-        assert(!checkJSONExpression({a: {$lt:  10}}, {a: 11}));
-        assert(!checkJSONExpression({a: {$lt:  10}}, {a: 10}));
-        assert( checkJSONExpression({a: {$lt:  10}}, {a: 9}));
+        assert(!cje({a: {$lt:  10}}, {a: 11}));
+        assert(!cje({a: {$lt:  10}}, {a: 10}));
+        assert( cje({a: {$lt:  10}}, {a: 9}));
 
-        assert(!checkJSONExpression({a: {$lte: 10}}, {a: 11}));
-        assert( checkJSONExpression({a: {$lte: 10}}, {a: 10}));
-        assert( checkJSONExpression({a: {$lte: 10}}, {a: 9}));
+        assert(!cje({a: {$lte: 10}}, {a: 11}));
+        assert( cje({a: {$lte: 10}}, {a: 10}));
+        assert( cje({a: {$lte: 10}}, {a: 9}));
         done();
     });
 });
