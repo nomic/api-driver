e6e16d5c20a13227cc51c144ad566ebcfb3b6b5a
Aaron Boyd <aaronyo@yahoo.com>
2014-03-29 02:09:27 +0000

    tests: white space tweaks

--- test/test_expector.js
+++ test/test_expector.js
@@ -8,42 +8,42 @@ var assert = require('assert'),
 
 suite('checkJSONExpresion', function() {
     test('$length', function(done) {
-        assert(cje({$length: 1}, [2]));
-        assert(!cje({$length: 1}, []));
-        assert(!cje({$length: 1}, [2, 3]));
-        assert(cje({$length: 4}, [1, 2, 3, 4]));
+        assert(  cje({$length: 1}, [2]) );
+        assert( !cje({$length: 1}, []) );
+        assert( !cje({$length: 1}, [2, 3]) );
+        assert(  cje({$length: 4}, [1, 2, 3, 4]) );
 
         done();
     });
 
     test('$unordered', function(done) {
-        assert(cje({$unordered: [1, 2]}, [1, 2]));
-        assert(cje({$unordered: [1, 2]}, [2, 1]));
+        assert( cje({$unordered: [1, 2]}, [1, 2]) );
+        assert( cje({$unordered: [1, 2]}, [2, 1]) );
 
         // FIXME: this seems like it should fail
         //assert(!cje({$unordered: [1, 2]}, [1, 2, 3]));
 
-        assert(!cje({$unordered: [1, 2, 3]}, [1, 2]));
+        assert( !cje({$unordered: [1, 2, 3]}, [1, 2]) );
 
         done();
     });
 
     test('comparisons', function(done) {
-        assert( cje({a: {$gt:  10}}, {a: 11}));
-        assert(!cje({a: {$gt:  10}}, {a: 10}));
-        assert(!cje({a: {$gt:  10}}, {a: 9}));
+        assert(  cje({a: {$gt:  10}}, {a: 11}) );
+        assert( !cje({a: {$gt:  10}}, {a: 10}) );
+        assert( !cje({a: {$gt:  10}}, {a: 9})) ;
 
-        assert( cje({a: {$gte: 10}}, {a: 11}));
-        assert( cje({a: {$gte: 10}}, {a: 10}));
-        assert(!cje({a: {$gte: 10}}, {a: 9}));
+        assert(  cje({a: {$gte: 10}}, {a: 11}) );
+        assert(  cje({a: {$gte: 10}}, {a: 10}) );
+        assert( !cje({a: {$gte: 10}}, {a: 9})) ;
 
-        assert(!cje({a: {$lt:  10}}, {a: 11}));
-        assert(!cje({a: {$lt:  10}}, {a: 10}));
-        assert( cje({a: {$lt:  10}}, {a: 9}));
+        assert( !cje({a: {$lt:  10}}, {a: 11}) );
+        assert( !cje({a: {$lt:  10}}, {a: 10}) );
+        assert(  cje({a: {$lt:  10}}, {a: 9})) ;
 
-        assert(!cje({a: {$lte: 10}}, {a: 11}));
-        assert( cje({a: {$lte: 10}}, {a: 10}));
-        assert( cje({a: {$lte: 10}}, {a: 9}));
+        assert( !cje({a: {$lte: 10}}, {a: 11}) );
+        assert(  cje({a: {$lte: 10}}, {a: 10}) );
+        assert(  cje({a: {$lte: 10}}, {a: 9})) ;
         done();
     });
 });
