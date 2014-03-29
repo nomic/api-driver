9d760fd92e1cce637f5b0a3027ca7cfe378c3842
Aaron Boyd <aaronyo@yahoo.com>
2014-03-29 02:12:22 +0000

    fix whitespace to .jshintrc specifed 2

--- test/test_expector.js
+++ test/test_expector.js
@@ -7,43 +7,43 @@ var assert = require('assert'),
 
 
 suite('checkJSONExpresion', function() {
-    test('$length', function(done) {
-        assert(  cje({$length: 1}, [2]) );
-        assert( !cje({$length: 1}, []) );
-        assert( !cje({$length: 1}, [2, 3]) );
-        assert(  cje({$length: 4}, [1, 2, 3, 4]) );
+  test('$length', function(done) {
+    assert(  cje({$length: 1}, [2]) );
+    assert( !cje({$length: 1}, []) );
+    assert( !cje({$length: 1}, [2, 3]) );
+    assert(  cje({$length: 4}, [1, 2, 3, 4]) );
 
-        done();
-    });
+    done();
+  });
 
-    test('$unordered', function(done) {
-        assert( cje({$unordered: [1, 2]}, [1, 2]) );
-        assert( cje({$unordered: [1, 2]}, [2, 1]) );
+  test('$unordered', function(done) {
+    assert( cje({$unordered: [1, 2]}, [1, 2]) );
+    assert( cje({$unordered: [1, 2]}, [2, 1]) );
 
-        // FIXME: this seems like it should fail
-        //assert(!cje({$unordered: [1, 2]}, [1, 2, 3]));
+    // FIXME: this seems like it should fail
+    //assert(!cje({$unordered: [1, 2]}, [1, 2, 3]));
 
-        assert( !cje({$unordered: [1, 2, 3]}, [1, 2]) );
+    assert( !cje({$unordered: [1, 2, 3]}, [1, 2]) );
 
-        done();
-    });
+    done();
+  });
 
-    test('comparisons', function(done) {
-        assert(  cje({a: {$gt:  10}}, {a: 11}) );
-        assert( !cje({a: {$gt:  10}}, {a: 10}) );
-        assert( !cje({a: {$gt:  10}}, {a: 9})) ;
+  test('comparisons', function(done) {
+    assert(  cje({a: {$gt:  10}}, {a: 11}) );
+    assert( !cje({a: {$gt:  10}}, {a: 10}) );
+    assert( !cje({a: {$gt:  10}}, {a: 9})) ;
 
-        assert(  cje({a: {$gte: 10}}, {a: 11}) );
-        assert(  cje({a: {$gte: 10}}, {a: 10}) );
-        assert( !cje({a: {$gte: 10}}, {a: 9})) ;
+    assert(  cje({a: {$gte: 10}}, {a: 11}) );
+    assert(  cje({a: {$gte: 10}}, {a: 10}) );
+    assert( !cje({a: {$gte: 10}}, {a: 9})) ;
 
-        assert( !cje({a: {$lt:  10}}, {a: 11}) );
-        assert( !cje({a: {$lt:  10}}, {a: 10}) );
-        assert(  cje({a: {$lt:  10}}, {a: 9})) ;
+    assert( !cje({a: {$lt:  10}}, {a: 11}) );
+    assert( !cje({a: {$lt:  10}}, {a: 10}) );
+    assert(  cje({a: {$lt:  10}}, {a: 9})) ;
 
-        assert( !cje({a: {$lte: 10}}, {a: 11}) );
-        assert(  cje({a: {$lte: 10}}, {a: 10}) );
-        assert(  cje({a: {$lte: 10}}, {a: 9})) ;
-        done();
-    });
+    assert( !cje({a: {$lte: 10}}, {a: 11}) );
+    assert(  cje({a: {$lte: 10}}, {a: 10}) );
+    assert(  cje({a: {$lte: 10}}, {a: 9})) ;
+    done();
+  });
 });
