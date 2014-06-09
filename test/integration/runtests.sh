6069070bfdafc993f40960e61100ae2b658351d5
Aaron Boyd <aaronyo@yahoo.com>
2014-06-09 00:48:53 -0700

    fix test runner console output

--- test/integration/runtests.sh
+++ test/integration/runtests.sh
@@ -7,15 +7,16 @@ echo ""
 echo "## Launching dummy app"
 node $SCRIPT_DIR/dummy_app/app > /dev/null &
 
-echo ""
-echo "## Running tests"
 # wait for dummy app to come up
 while true
 do
   curl --silent localhost:$DUMMY_APP_PORT > /dev/null && break || echo -n "."
   sleep 0.1
 done
+echo ""
 
+echo ""
+echo "## Running tests"
 mocha --ui tdd --reporter spec $SCRIPT_DIR/test_*.js
 
 echo "## Killing dummy app"
